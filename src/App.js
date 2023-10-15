da3adfa669a534d74fed5434e28c360572339d17
samira hossain <samira.hossain@outlook.com>
2023-10-16 01:24:02 +1000

    to do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Checkbox from "./checkbox";
 import './App.css';
 
-function App() {
+const Todo = () => {
+  const [task, setTask] = useState({
+    id: 0,
+    taskDescription: "",
+    isCompleted: false
+  });
+
+  const [todos, setTodos] = useState([]);
+
+  function addTodos(e) {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: todos.length + 1,
+        taskDescription: task.taskDescription,
+        isCompleted: false
+      }
+    ]);
+
+    setTask({
+      id: 0,
+      taskDescription: "",
+      isCompleted: false
+    });
+  }
+
+  const handleToggle = (id) => {
+    console.log({ id })
+    let mapped = todos.map((task) => {
+      return task.id === Number(id)
+        ? { ...task, isCompleted: true }
+        : { ...task };
+    });
+    setTodos(mapped);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To Do list</h1>
+      <form onSubmit={addTodos}>
+        <div>
+          <label>
+            Tasks for today &nbsp;
+            <input
+              type="text"
+              value={task.taskDescription}
+              name="taskDescription"
+              onChange={(event) =>
+                setTask({
+                  taskDescription: event.target.value,
+                  isCompleted: false
+                })
+              }
+            />
+          </label>
+          <button>Add To Do</button>
+        </div>
+      </form>
+      <>
+        {todos.length > 0 ? (
+          <>
+            {todos.map((t, index) => {
+              return (
+                <div id={t.id} key={index + t.id} value={t.id}>
+                  {t.isCompleted ? (
+                    <strike>
+                      <p>{t.taskDescription}</p>
+                    </strike>
+                  ) : (
+                    <Checkbox
+                      label={t.taskDescription}
+                      value={t.id}
+                      checked={t.isCompleted}
+                      onChange={(e) => handleToggle(t.id)}
+                    />
+                  )}
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <p>No tasks for today</p>
+        )}
+      </>
     </div>
   );
-}
+};
 
-export default App;
+export default Todo;
